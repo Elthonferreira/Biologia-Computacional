0fe94c31ab977041d3f1adfd404a8e5972b0ba84
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-05 15:32:46 -0300

    questao 2

---
+++ src/Transcribing_dna_to_rna.java
@@ -0,0 +1,20 @@
+
+public class Transcribing_dna_to_rna {
+
+	public static void main(String[] args) {
+		String dna = "GATGGAACTTGACTACGTAAATT";
+		transcribing_dna_to_rna(dna);
+	}
+	
+	public static void transcribing_dna_to_rna (String dna) {
+		
+		if (dna.length() > 1000) {
+			System.out.println("Tamanho máximo da sequência permitido = 1000 nt.");
+		}
+		String rna = null;
+		for (int i = 0; i < dna.length(); i++) {
+			rna = dna.replace('T', 'U');
+		}	
+		System.out.println(rna);
+	}
+}
