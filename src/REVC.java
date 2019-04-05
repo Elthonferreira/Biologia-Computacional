ae365b9461fca7fc70f015e8c806a34a11220b5e
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-05 15:40:02 -0300

    questao 3

---
+++ src/REVC.java
@@ -0,0 +1,13 @@
+
+public class REVC {
+
+	public static void main(String[] args) {
+		String dna = "AAAACCCGGT";
+		complementing_a_strand_of_dna(dna);
+	}
+	
+	public static void complementing_a_strand_of_dna (String dna) {
+		
+	}
+
+}
