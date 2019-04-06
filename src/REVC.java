ad47ac73be16d27c226d40b80023faada10152d3
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-06 00:19:26 -0300

    questao3

--- src/REVC.java
+++ src/REVC.java
@@ -12,22 +12,33 @@ public class REVC {
 			System.out.println("Tamanho máximo da sequência permitido = 1000 nt.");
 			System.exit(0);
 		}
-		System.out.println("String original:   " + dna);
-		// Substituição das letras
-		String rna = "";
 		
-		//for (int i = 0; i < dna.length(); i++) {
-			rna = dna.replace('A', 'T');
-			rna = dna.replace('T', 'A');
-			
-		
-		System.out.println("String modificada: " + rna);
+		//System.out.println("String original:   " + dna);
 		
 		// String ao contrário
 		String reverso = "";
 		for (int j = dna.length() - 1; j >= 0; j--) {
 			reverso = reverso + dna.charAt(j);
 		}
-		System.out.println("String reversa:    " + reverso);
+		
+		//System.out.println("String reversa:    " + reverso);
+		// Colocando a string invertida no vetor1
+		char vetor1 [] = new char[dna.length()];
+		char vetor2 [] = new char [dna.length()];
+		for (int i = 0; i < dna.length(); i++) {
+			vetor1[i] = reverso.charAt(i);
+		}
+		// Substituição das letras
+		for (int i = 0; i < dna.length(); i++) {
+			if (vetor1[i] == 'A') {vetor2[i] = 'T';}
+			else if (vetor1[i] == 'T') {vetor2[i] = 'A';}
+			else if (vetor1[i] == 'C') {vetor2[i] = 'G';}
+			else if (vetor1[i] == 'G') {vetor2[i] = 'C';}
+			else {continue;}
+		}
+		// Impressão
+		for (int i = 0; i < dna.length(); i++) {
+			System.out.print(vetor2[i]);
+		}
 	}
 }
