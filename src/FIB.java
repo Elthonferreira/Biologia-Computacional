8092a3da1ba6f0d4f8d7d931182ee85c515ef171
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-07 22:13:51 -0300

    questao 4

--- src/FIB.java
+++ src/FIB.java
@@ -2,11 +2,16 @@
 public class FIB {
 
 	public static void main(String[] args) {
-		
-
+		int fib = 12;
+		System.out.println(rabbits_and_recurrence_relations(fib));
 	}
 	
-	public static void rabbits_and_recurrence_relations () {
-		
+	public static int rabbits_and_recurrence_relations (int fib) {
+		if (fib < 2) {
+			return fib;
+		}
+		else {
+			return rabbits_and_recurrence_relations(fib - 1) + rabbits_and_recurrence_relations(fib - 2);
+		}
 	}
 }
