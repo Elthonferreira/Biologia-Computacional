ac18ea3ced3b98f249f89322c0563100b8466a16
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-12 20:55:04 -0300

    questão 4

--- src/FIB.java
+++ src/FIB.java
@@ -1,17 +1,30 @@
-
+// Feito por Elthon Ferreira
 public class FIB {
 
-	public static void main(String[] args) {
-		int fib = 12;
-		System.out.println(rabbits_and_recurrence_relations(fib));
-	}
+	private static long contador; // Contador global
 	
-	public static int rabbits_and_recurrence_relations (int fib) {
-		if (fib < 2) {
-			return fib;
+	public static void main(String[] args) {	
+		long mes = 5, pares = 3;
+		// Condition Positive integers n <= 40 and k <= 5.
+		if (mes > 40 || pares > 5) {System.out.println("Escolha valores com a quantidade de"
+									+ "meses menores que 40 e pares menores que 5!");
+		System.exit(0);	// Kill program
 		}
-		else {
-			return rabbits_and_recurrence_relations(fib - 1) + rabbits_and_recurrence_relations(fib - 2);
+		System.out.println(rabbits_and_recurrence_relations(mes, pares)); // Call and print function
+	}
+	public static long aux(long mes, long pares) {
+			if (mes < 3) return 1;								// Caso base
+			else if (mes == 3 || mes == 4) return pares;		// Caso base
+			else {
+				return aux(mes - 1, pares) + 					// Caso geral
+					  (aux(mes - 2, pares)*pares);
+			}
+	}
+	public static long rabbits_and_recurrence_relations(long mes, long pares) {
+		for (long i = 1; i <= mes; i ++) {				
+			contador += aux(i, pares);							// Contador
+			//System.out.println("(" + i + "):" + aux(i, pares));	
 		}
+		return contador - 1;
 	}
 }
