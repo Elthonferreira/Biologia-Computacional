de8319c783d70ed265addf98ea06bbecae43da5e
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-13 02:49:22 -0300

    Questão 1

--- src/DNA.java
+++ src/DNA.java
@@ -1,4 +1,4 @@
-
+// By Elthon Ferreira
 public class DNA {
 
 	public static void main (String args[]) {
@@ -13,21 +13,11 @@ public class DNA {
 		// Incremento nas letras achadas
 		int a = 0, c = 0, t = 0, g = 0;
 		for (int i = 0; i < dna.length(); i++) {
-			if (dna.charAt(i) == 'A') {
-				a++;
-			}
-			else if (dna.charAt(i) == 'C') {
-				c++;
-			}
-			else if (dna.charAt(i) == 'G') {
-				g++;
-			}
-			else if (dna.charAt(i) == 'T') {
-				t++;
-			}
-			else {
-				continue;
-			}
+			if (dna.charAt(i) == 'A') 		a++;
+			else if (dna.charAt(i) == 'C')	c++;
+			else if (dna.charAt(i) == 'G') 	g++;
+			else if (dna.charAt(i) == 'T') 	t++;
+			else continue;
 		}
 		System.out.println("\n" + a + " " + c + " " + g + " " + t); // Impressão formatada
 	}
